c922cf7c1b58b71249823acbf7b6576b6e6b72ed
Val <vdecyatnik@gmail.com>
2021-01-18 19:27:26 -0800

    file

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
+} else {
 
   connection = mysql.createConnection({
     host: "localhost",
@@ -14,16 +14,16 @@ if(process.env.JAWSDB_URL){
     password: "1235K",
     database: "burgers_db",
 
-});
+  });
+};
 
 
-
-connection.connect(function (err) {
+  connection.connect(function (err) {
     if (err) {
       console.error("error connecting: " + err.stack);
       return;
     }
     console.log("connected as id " + connection.threadId);
-  });}
+  });
 
   module.exports = connection;
